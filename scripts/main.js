240163e98c7fd8810d2eedf77dc183afde99fb55
yuyiju <56580851+yuyiju@users.noreply.github.com>
2019-10-15 15:54:36 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+/*
+Everything in between is a comment.
+*/
+
+// This is a comment 
+
+var myImage = document.querySelector('img');
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/main.png') {
+      myImage.setAttribute ('src','images/main2.png');
+    } else {
+      myImage.setAttribute ('src','images/main.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Grass! Mud! Horse!!!' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'kkkkkk, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
